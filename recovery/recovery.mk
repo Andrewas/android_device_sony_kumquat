90f445e175721003f5b1ec3893662f8f752c14fe
Rebel <gahlotpercy@gmail.com>
2013-10-02 17:39:54 +0530

    Delete recovery.mk

--- recovery/recovery.mk
+++
@@ -1,5 +0,0 @@
-# Recovery resources
-PRODUCT_COPY_FILES += \
-    bootable/recovery/res/images/icon_clockwork.png:root/res/images/icon_clockwork.png \
-    bootable/recovery/res/images/stitch.png:root/res/images/stitch.png
-
