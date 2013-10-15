8da95c17fbb0c8be741550e563ba22d423643500
Gava97 <gava97@gmail.com>
2013-10-15 15:48:07 +0200

    Update custombootimg.mk

--- custombootimg.mk
+++ custombootimg.mk
@@ -7,8 +7,6 @@ $(INSTALLED_BOOTIMAGE_TARGET): $(PRODUCT_OUT)/kernel $(recovery_ramdisk) $(INSTA
 	$(hide) mkdir -p $(PRODUCT_OUT)/combinedroot/
 	$(hide) cp -R $(PRODUCT_OUT)/root/* $(PRODUCT_OUT)/combinedroot/
 	$(hide) cp -R $(PRODUCT_OUT)/../../../../device/sony/kumquat/config/root/default.prop $(PRODUCT_OUT)/combinedroot/
-	$(hide) sed -i 's/18D1/0FCE/g' $(PRODUCT_OUT)/recovery/root/init.rc
-	$(hide) sed -i 's/D001/617E/g' $(PRODUCT_OUT)/recovery/root/init.rc
 	$(hide) $(MKBOOTFS) $(PRODUCT_OUT)/recovery/root > $(PRODUCT_OUT)/recoveryforkexec.cpio
 	$(hide) cat $(PRODUCT_OUT)/recoveryforkexec.cpio | gzip > $(PRODUCT_OUT)/recoveryforkexec.fs
 	$(hide) cp -R $(PRODUCT_OUT)/recoveryforkexec.fs $(PRODUCT_OUT)/combinedroot/sbin/recovery_ramdisk.gz
