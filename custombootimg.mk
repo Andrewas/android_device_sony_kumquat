654a027dabe3cb0d0069f1667267245e85918c72
Gava97 <gava97@gmail.com>
2013-10-20 18:42:42 +0200

    Update custombootimg.mk

--- custombootimg.mk
+++ custombootimg.mk
@@ -7,7 +7,7 @@ $(INSTALLED_BOOTIMAGE_TARGET): $(PRODUCT_OUT)/kernel $(recovery_ramdisk) $(INSTA
 	$(hide) mkdir -p $(PRODUCT_OUT)/combinedroot/
 	$(hide) cp -R $(PRODUCT_OUT)/root/* $(PRODUCT_OUT)/combinedroot/
 	$(hide) cp -R $(PRODUCT_OUT)/../../../../device/sony/kumquat/config/root/default.prop $(PRODUCT_OUT)/combinedroot/
-        $(hide) cp -R $(PRODUCT_OUT)/../../../../device/sony/kumquat/recovery/init.rc $(PRODUCT_OUT)/recovery/root/ 
+        $(hide) cp -R $(PRODUCT_OUT)/../../../../device/sony/lotus/recovery/init.rc $(PRODUCT_OUT)/recovery/root/
 	$(hide) $(MKBOOTFS) $(PRODUCT_OUT)/recovery/root > $(PRODUCT_OUT)/recoveryforkexec.cpio
 	$(hide) cat $(PRODUCT_OUT)/recoveryforkexec.cpio | gzip > $(PRODUCT_OUT)/recoveryforkexec.fs
 	$(hide) cp -R $(PRODUCT_OUT)/recoveryforkexec.fs $(PRODUCT_OUT)/combinedroot/sbin/recovery_ramdisk.gz
